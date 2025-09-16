2f76682a9cb5e9e9b5673dd8b0d441fd399af5a7
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-09-17 00:46:40 +0530

    add map transformation

--- spark_core/Transformations/Transformations.py
+++ spark_core/Transformations/Transformations.py
@@ -10,4 +10,5 @@ dist_words = sc.parallelize(words)
 word_lengths = dist_words.map(lambda word: len(word))
 
 print(word_lengths.collect())
+# note reduce is an action
 print("total world length is: ", word_lengths.reduce(lambda a, b: a + b))    
