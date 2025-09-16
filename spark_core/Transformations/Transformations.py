3f96e38960f465aefad7ee1e561d1a1cc8e8f602
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-09-17 00:46:35 +0530

    add map transformation

---
+++ spark_core/Transformations/Transformations.py
@@ -0,0 +1,13 @@
+import pyspark
+
+sc = pyspark.SparkContext("local", "my-app")
+
+words = ["mercedes", "volvo"]
+dist_words = sc.parallelize(words)
+
+
+# map is a transformation here
+word_lengths = dist_words.map(lambda word: len(word))
+
+print(word_lengths.collect())
+print("total world length is: ", word_lengths.reduce(lambda a, b: a + b))    
