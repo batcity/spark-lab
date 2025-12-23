a31e0aed28a0830bfc234c866b8b24e6fbde6958
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-12-24 00:51:49 +0530

    add example for structured streaming

--- spark_streaming/structured_streaming/structured_streaming.py
+++ spark_streaming/structured_streaming/structured_streaming.py
@@ -1,1 +1,12 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pyspark.sql.functions import when
+
+spark = SparkSession.builder.appName("StructuredStreamingExample").getOrCreate()
+
+temperatures = spark.readStream.format("socket").option("host","localhost").option("port","9999").load()
+temps_formatted = temperatures.select(col("value").cast("double").alias("temperature"))
+temps_filtered_with_message = temps_formatted.select(col("temperature"),when(col("temperature") > 50,"warning - temperature is above 50 degrees!").alias("message"))
+
+query = temps_filtered_with_message.writeStream.format("console").option("truncate", False).start()
+query.awaitTermination()
